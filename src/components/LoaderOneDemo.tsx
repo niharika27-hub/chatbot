9abb079f9751c3973ea602029fca4258f67ce697
Ankush Thakur <k41946688@gmail.com>
2025-08-22 14:15:50 +0530

    [dyad] wrote 1 file(s)

--- src/components/LoaderOneDemo.tsx
+++ src/components/LoaderOneDemo.tsx
@@ -4,9 +4,5 @@ import React from "react";
 import { LoaderOne } from "@/components/ui/loader";
 
 export function LoaderOneDemo() {
-  return (
-    <div className="flex justify-center items-center h-screen">
-      <LoaderOne />
-    </div>
-  );
+  return <LoaderOne />;
 }
