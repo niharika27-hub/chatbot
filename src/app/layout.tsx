23db6da5a4a89001ab7ec241b556f4b84ac85e54
Ankush Thakur <k41946688@gmail.com>
2025-08-22 14:46:42 +0530

    [dyad] wrote 1 file(s) + extra files edited outside of Dyad

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased h-screen overflow-hidden`}
       >
         {children}
         <Toaster /> {/* Add Toaster component here */}
