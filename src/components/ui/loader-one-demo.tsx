1fa9f40d64fb899f23ef9640fbf548aa2b30ceaa
Ankush Thakur <k41946688@gmail.com>
2025-08-22 14:16:33 +0530

    [dyad] wrote 1 file(s), renamed 1 file(s)

--- src/components/ui/loader-one-demo.tsx
+++ src/components/ui/loader-one-demo.tsx
@@ -1,8 +1,6 @@
-"use client";
-
 import React from "react";
 import { LoaderOne } from "@/components/ui/loader";
 
-export function LoaderOneDemo() {
+export default function LoaderOneDemo() {
   return <LoaderOne />;
 }
